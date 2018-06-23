893c215af99e4753dfde95ca095f244f2b959fb9
ava krishn <avakrishn@gmail.com>
2018-06-22 21:43:31 -0700

    added the game characters as objects in an array in javascript file

---
+++ assets/javascript/game.js
@@ -0,0 +1,52 @@
+var characters= [
+    {
+        name: 'Captain',
+        health: 180,
+        attack: 20,
+        fullImageUrl: "assets/images/captain.png",
+        faceImgUrl: "assets/images/captain-headshot.png",
+        enemyAttackBack: 25
+    },
+    {
+        name: 'Helmswoman',
+        health: 120,
+        attack: 8,
+        fullImageUrl: "assets/images/helmswoman.png",
+        aceImgUrl: "assets/images/helmswoman-headshot.png",
+        enemyAttackBack: 10
+    },
+
+    {
+        name: 'Quartermaster',
+        health: 140,
+        attack: 14,
+        fullImageUrl: "assets/images/quartermaster-right.png",
+        aceImgUrl: "assets/images/quartermaster-headshot.png",
+        enemyAttackBack: 15
+    },
+
+    {
+        name: 'First mate',
+        health: 160,
+        attack: 20,
+        fullImageUrl: "assets/images/firstmate.png",
+        faceImgUrl: "assets/images/firstmate-headshot.png",
+        enemyAttackBack: 20
+    },
+
+    {
+        name: 'Cook',
+        health: 100,
+        attack: 8,
+        fullImageUrl: "assets/images/cook.png",
+        faceImgUrl: "assets/images/cook-headshot.png",
+        enemyAttackBack: 5
+    },
+]
+
+// $(document).ready(function(){
+    
+
+
+// });
+
